eb373f44b3cd5cefb97ac18640653d22076e2439
hrugved <whrugved@gmail.com>
2022-07-20 01:34:46 +0530

    add script

--- contracts/test/BasicNft.sol
+++ contracts/test/BasicNft.sol
@@ -7,15 +7,17 @@ contract BasicNft is ERC721 {
   string public constant TOKEN_URI =
     "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
   uint256 private s_tokenCounter;
-
+ 
+  event DogMinted(uint256 indexed tokenId);
+  
   constructor() ERC721("Dogie", "DOG") {
     s_tokenCounter = 0;
   }
 
-  function mintNft() public returns (uint256) {
+  function mintNft() public {
     _safeMint(msg.sender, s_tokenCounter);
+    emit DogMinted(s_tokenCounter);
     s_tokenCounter++;
-    return s_tokenCounter;
   }
 
   function tokenURI(uint256) public pure override returns (string memory) {
