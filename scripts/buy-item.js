6f5ac980a4cb816884bb4b519f0173a080e0de13
hrugved <whrugved@gmail.com>
2022-07-20 20:41:25 +0530

    add more scripts

---
+++ scripts/buy-item.js
@@ -0,0 +1,24 @@
+const { ethers, network } = require("hardhat")
+const { moveBlocks, sleep } = require("./move-block")
+
+const TOKEN_ID = 5
+
+buyItem = async () => {
+  const nftMarketplace = await ethers.getContract("NftMarketplace")
+  const basicNft = await ethers.getContract("BasicNft")
+  const listing = await nftMarketplace.getListing(basicNft.address, TOKEN_ID)
+  const price = listing.price.toString()
+  const tx = await nftMarketplace.buyItem(basicNft.address, TOKEN_ID, { value: price })
+  await tx.wait(1)
+  console.log("Bought NFT!")
+  if (network.config.chainId == "31337") {
+    await moveBlocks(2, (sleepAmount = 1000))
+  }
+}
+
+buyItem()
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
---
+++ scripts/cancel-item.js
@@ -0,0 +1,24 @@
+const { ethers, network } = require("hardhat")
+const { moveBlocks, sleep } = require("./move-block")
+
+const PRICE = ethers.utils.parseEther('0.1')
+
+const TOKEN_ID = 6
+
+cancelItem = async () => {
+  const nftMarketplace = await ethers.getContract("NftMarketplace")
+  const basicNft = await ethers.getContract("BasicNft")
+  const tx = await nftMarketplace.cancelListing(basicNft.address,TOKEN_ID)
+  await tx.wait(1)
+  console.log('NFT Cancelled!')  
+  if(network.config.chainId=='31337') {
+    await moveBlocks(2,(sleepAmount=1000))
+  }
+}
+
+cancelItem()
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
